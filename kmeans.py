f36ab54941af68086efc1e1fe7d6642117cc6269
aliwalker <aliwalker880@gmail.com>
2020-01-01 10:56:05 +0800

    k-means.

--- kmeans.py
+++ kmeans.py
@@ -36,8 +36,16 @@ def elbow_method(X):
     plt.show()
 
 def main():
+    '''
+    Use 4 as the chosen number of clusters.
+    '''
     X, y = gen_sample()
-    elbow_method(X)
+    kmeans = KMeans(n_clusters=4, init='k-means++', max_iter=300, n_init=10, random_state=0)
+    pred_y = kmeans.fit_predict(X)
+
+    plt.scatter(X[:, 0], X[:, 1])
+    plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='red')
+    plt.show()
 
 if __name__ == '__main__':
     main()
